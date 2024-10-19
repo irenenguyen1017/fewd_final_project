20ec379e38630070821a7460f0b81b0dd0a1b1c8
Binh Nguyen (Irene) <98362650+irenenguyen1017@users.noreply.github.com>
2024-10-19 21:04:51 +1100

    implement BankAccountUser and BankAccountManager classes

--- src/main.js
+++ src/main.js
@@ -23,11 +23,81 @@ class StorageManager {
   // Get an item from localStorage
   get(key) {
     const value = localStorage.getItem(key);
+
     return value ? JSON.parse(value) : null;
   }
 }
 
+class BankAccountUser {
+  constructor({fullName, userName, transactions=[], interestRate, pin, accountNunber}) {
+    this.fullName = fullName;
+    this.userName = userName;
+    this.transactions = transactions;
+    this.interestRate = interestRate;
+    this.pin = pin;
+    this.accountNunber = accountNunber || this.generateAccountNumber();
+  }
+
+  generateAccountNumber() {
+    return Math.floor(Math.random() * 900000 + 100000);
+  }
+}
+
+class BankAccountManager {
+  constructor() {
+    this.storageKey = 'users'; // Key to store all user and bank account data in localStorage
+    this.currentUserKey = 'currentUser'; // Key for storing the logged-in user
+    this.timeoutDuration = 60 * 60 * 1000; // 1-hour session timeout duration
+    this.storage = new StorageManager(); // Use StorageManager for all localStorage operations
+  }
+
+  register({data, onSuccess, onError}) {
+    const users = this.getUsers();
+    const {fullName, userName, pin} = data;
+
+    console.log({ users})
+
+    const existingUser = users.find(user => user.userName === userName);
+    if (existingUser) {
+      onError('User already registered');
+      return;
+    }
+
+    const newUser = new BankAccountUser({fullName, userName, pin, interestRate: this.generateInterestRate()});
+
+    users.push(newUser);
+
+    this.setUsers(users);
+  }
+
+  getUsers() {
+    return this.storage.get(this.storageKey) ?? [];
+  }
+
+  setUsers(users) {
+    this.storage.set(this.storageKey, users);
+  }
+
+  getCurrentUser() {
+    return this.storage.get(this.currentUserKey);
+  }
+  
+  setCurrentUser(user) {
+    return this.storage.set(this.currentUserKey, user);
+  }
+
+  generateInterestRate() {
+    return (Math.random() * 3).toFixed(2);
+  }
+}
+
+
 function main() {
+  // Page elements
+  const dashboardPage = document.getElementById('dashboard-page');
+  const loginPage = document.getElementById('login-page');
+  const registerPage = document.getElementById('register-page');
+
   // Loader element
   const loader = document.getElementById('loader');
 
@@ -71,6 +141,27 @@ function main() {
   const deleteForm = document.getElementById('delete-form');
   const deleteAccountInput = document.getElementById('delete-account-input');
   const deleletAccountPasswordInput = document.getElementById('delete-account-password-input');
+
+  const bankAccountManager = new BankAccountManager();
+
+  function initialize() {
+  }
+  
+
+  bankAccountManager.register({
+    data: {
+      username: 'test',
+      fullName: 'John Smith',
+      pin: 1111
+    },
+    onError(errorMessage) {
+      console.log(errorMessage);
+    },
+    onSuccess() {
+      console.log('Account created succesfully!')
+    }
+
+  })
 }
 
 main();
